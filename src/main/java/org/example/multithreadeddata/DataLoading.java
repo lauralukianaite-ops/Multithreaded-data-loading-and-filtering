d8e14c925f0d847377e412c5caeb7d6a87fe3882
Laura <laura.lukianaite@gmail.com>
2026-05-15 10:48:14 +0300

    Starting reading from file functionality

--- src/main/java/org/example/multithreadeddata/DataLoading.java
+++ src/main/java/org/example/multithreadeddata/DataLoading.java
@@ -1,4 +1,41 @@
 package org.example.multithreadeddata;
 
-public class DataLoading {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataLoading implements Runnable{
+    private final List<Person> personList;
+    private final File file;
+    private final int number;
+
+    public DataLoading(int number){
+        personList = new ArrayList<>();
+        this.number = number;
+        this.file = new File("data/MOCK_DATA"+number+".csv");
+    }
+
+    @Override
+    public void run(){
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            boolean isFirstLine = true;
+            int count = 0;
+
+            while ((line = br.readLine()) != null){
+                if(isFirstLine){
+                    isFirstLine = false;
+                    continue;
+                }
+
+                Person = parseCsvLine(line);
+            }
+        }
+
+
+    }
+
+
 }
